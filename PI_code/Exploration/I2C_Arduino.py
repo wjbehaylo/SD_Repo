425c3dad4e2f3bfd5904a39594c9545a4dd8139e
wjbehaylo <103539849+wjbehaylo@users.noreply.github.com>
2025-02-13 01:10:46 -0700

    I believe the I2C_Arduino.py program should run from the pi's side without flaws. Now we just need to do the same on the Arduino, which shouldn't be too hard.

--- PI_code/Exploration/I2C_Arduino.py
+++ PI_code/Exploration/I2C_Arduino.py
@@ -1,56 +1,67 @@
 # Purpose: this is an exploration file to set up the Raspberry Pi to Arduino I2C sending.
 # Contributors: Walter
-# Sources: SEED_LAB repository, https://www.engineersgarage.com/articles-raspberry-pi-i2c-bus-pins-smbus-smbus2-python/
+# Sources: SEED_LAB repository for previous examples, install for SMBus2https://pypi.org/project/smbus2/ Documentation for smbus2 https://smbus2.readthedocs.io/en/latest/
 # Relevant files: this file is related to the I2C_Pi file located in ARD_code.
-# Circuitry: if the code is run using some type of wiring or external circuit, there should be a description of how to wire up that circuit, such that someone can recreate the full test.from smbus2 import SMBus
+# Circuitry: first, make sure smbus2 is downloaded on the pi. Run "pip install smbus2" to get it on
+# Then, we need the SMBUS libraryif the code is run using some type of wiring or external circuit, there should be a description of how to wire up that circuit, such that someone can recreate the full test.
+# we 
 
+from smbus2 import SMBus #this is to get the I2C connection functionality we want. We will need to run 
 from time import sleep
-import board
-import adafruit_character_lcd.character_lcd_rgb_i2c as character_lcd
-# I2C address of the Arduino, set in Arduino sketch
+# I2C address of the Arduino. I2C starts off by clarifying what is to be written to, then writing there.
 ARD_ADDR = 8
-# Initialize SMBus library with I2C bus 1
+# Initialize SMBus library with I2C bus 1. The Pi has 2 I2C busses it can connect, this is just saying we are using the first I believe.
 i2c_arduino = SMBus(1)
-lcd_columns = 16
-lcd_rows = 2
 
-i2c_lcd = board.I2C()
-lcd = character_lcd.Character_LCD_RGB_I2C(i2c_lcd, lcd_columns, lcd_rows)
-lcd.clear()
-lcd.color = [50, 0, 50]
+#offset represents which register will be written to on the arduino. Different registers might store different things based on mode of operation.
+#for instance, register 16 might be the I2C message pertaining to the rotation that should be gone to, whereas register 15 might have the amount to close the arms and register 14 might signify that capture is going
+#for now, we are just testing sending one message and we want to experiment with where to send it.
+write_offset=int(input("Enter an offset to write to:"))
 
+#this is the actual message that will be sent. 32 characters or less because 32 bytes send in one block.
+write_string=input("Enter a string of 32 characters or less:")
 
-# Do in a loop
-while(True):
-    mode = int(input("Which mode should the PI send in? \nDemonstration 1A (1)\nDemonstration 1B (2)\n"))
-    # Get user input for offset
-    offset = int(input("Enter an offset (7 to quit): "))
-    # Provide an exit key
-    if(offset == 7):
-        break
-    if(mode == 1):
-        # Get user input for command
-        string = input("Enter a string of 32 characters of less:")
-        # Write a byte to the i2c bus
-        command = [ord(character) for character in string]
-        try:
-            #ask arduino to take encoder reading
-            i2c_arduino.write_i2c_block_data(ARD_ADDR, offset, command)
-        except IOError:
-            print("Could not write data to the arduino")
-    elif(mode == 2):
-        #get user input for command
-        command = int(input("Enter an integer between 0 and 100: "))
-        while(command > 100 or command <0):
-            print("Out of range, enter a new input")
-            command = int(input("Enter an integer between 0 and 100: "))
-        try:
-            #aask arduino to take encoder reading
-            i2c_arduino.write_byte_data(ARD_ADDR, offset, command)
-            sleep(.1)
-            reply=i2c_arduino.read_byte_data(ARD_ADDR, offset)
-            print("Received from Arduino:" + str(reply))
-            lcd.message = str(reply)
-            sleep(10)
-        except IOError:
-            print("Could not write data to the arduino")
+#this is just a sanity check on what is trying to be sent
+#Also, I'm pretty sure that the '+' sign makes its own space, but I might have to add one in.
+print("sending"+ write_string)
+
+#the 'ord' function takes in a byte in the form of a character or length 1 string, and returns the unicode value
+#obviously, we are sending these in byte packages so we need to send them as their unicode values. 
+#the [] around the value of the message variable signifies that it will be an array, with an element for each character in the string being sent
+#this could be different types of things if we wanted to send pure numbers, in which case the byte values would be configured accordingly
+write_message= [ord(character) for character in write_string]
+
+#here we actually try to send the data over the line.
+#note that we threw it in a try except statement to indicate if the sending was successful from the Pi side of it
+try:
+    i2c_arduino.write_i2c_block_data(ARD_ADDR, write_offset, write_message)
+except IOError:
+    print("Could not write to the Arduino")
+
+#sanity check
+print("sending complete")
+
+#it would be easy to extend this to send bytes, all we would need to do is make message an integer input, potentially -128 - 127 to be sent with a byte
+#then, we would just use 'i2c_arduino.write_byte_data(ARD_ADDR, offset, command)'
+
+#this is just waiting for the user to try to get a message from the arduino side of things
+while(int(input("Enter 1 if the Arduino is ready to be read:")) !=1):
+    sleep(0.1)
+
+#note that this is not wrapped in a 'try-except' block, but it could be if that is deemed necessary and easy enough. It will be actually, just not right now.
+
+#I'm not entirely sure if this is necessary, we might just be able to read in all 32 bytes (which I think is the max of a block), and some will be irrelevant. Could be tested out though
+read_message_length_offset=int(input("What offset will have the length of the message being sent?"))
+read_message_length=i2c_arduino.read_byte_data(ARD_ADDR,read_message_length_offset)
+
+#now that we know the length of the message, which I'm still not sure if we needed or not, but is still useful. We now need where the message starts
+read_message_offset=int(input("Enter an offset to read from:"))
+read_message=i2c_arduino.read_i2c_block_data(ARD_ADDR,read_message_offset, read_message_length)
+
+#now we will print the length and location of the message, following by the message itself
+print("We read a message of length"+str(read_message_length)+"from offset"+read_message_offset+"of the Arduino:")
+print(str(read_message))
+
+#closing message
+print("This program is finished running now")
+i2c_arduino.close() #probably best practice
--- PI_code/Exploration/README.md
+++ PI_code/Exploration/README.md
@@ -5,7 +5,11 @@ Directory containing experimental/explorative code. Useful to learn more about h
 hello-world.py: a basic Python file to print hello world
 hello-world.cpp: a basic C++ file to print hello world
 
+I2C_Arduino.py: the pi's side of I2C communication with the Arduino. It includes both sending and receiving messages.
 
 cmd-led.py: a python file to turn on an external LED when an internal signal is asserted
 
 ultrasonic-led.py: a python file that should turn on a cascade of LEDs based on the distance detected by an ultrasonic sensor
+
+UART.py: sending UART messages between PUTTY on a laptop and the pi itself over a wired line
+
