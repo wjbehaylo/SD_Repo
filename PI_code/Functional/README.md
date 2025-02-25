3d8a1fd3545b6f2e73d4577b6de83738d699fb0c
wjbehaylo <103539849+wjbehaylo@users.noreply.github.com>
2025-02-25 09:11:33 -0700

    all that has changed is that we are doing integration on time of flight sensor

--- PI_code/Functional/README.md
+++ PI_code/Functional/README.md
@@ -1,3 +1,5 @@
 #README
 
 Directory containing code that will be functionally used, at least on a conceptual level, by the final deliverable.
+
+VL53L0X-sensor-code is a functional file that has the raspberry pi get a distance measurement from the I2C connected time of flight sensor
---
+++ PI_code/Functional/VL53L0X-sensor-code.py
@@ -0,0 +1,42 @@
+# Purpose: this is a functional file to set up the Raspberry Pi to Vilros time of flight sensor
+# Contributors: Walter, Angela
+# Sources: 
+#   https://github.com/adafruit/Adafruit_CircuitPython_VL53L0X python usage
+#   https://cdn-learn.adafruit.com/downloads/pdf/adafruit-vl53l0x-micro-lidar-distance-sensor-breakout.pdf sensor datasheet
+# Relevant files: 
+# Circuitry: connect the following listed pins
+#   top left pin on pi is 3.3V, connect to Vin
+#   second pin from top on left (03) is SDA on Pi,
+#   third pin from top on left (05) is SCL on Pi,
+#   fifth pin from top left (09) is ground on Pi.
+#   sensor has a voltage regulator, so Vin can be 5V-3V and it will work
+
+#   Must install the Adafruit Circuit Python VL53L0X, ensure latest version of Adafruit Circuit Python is running
+#   Ensure adafruit_vl53l0x.mpy and adafruit_bus_device libraries installed
+#   run sudo pip3 install adafruit-circuitpython-vl53l0x from command line
+
+import time
+
+import board
+import busio
+
+import adafruit_vl53l0x
+
+# Initialize I2C bus and sensor.
+#Note that the I2C address will be 0x29, or 41
+i2c = busio.I2C(board.SCL, board.SDA)
+vl53 = adafruit_vl53l0x.VL53L0X(i2c)
+
+# Optionally adjust the measurement timing budget to change speed and accuracy.
+# See the example here for more details:
+#   https://github.com/pololu/vl53l0x-arduino/blob/master/examples/Single/Single.ino
+# For example a higher speed but less accurate timing budget of 20ms:
+vl53.measurement_timing_budget = 20000
+# Or a slower but more accurate timing budget of 200ms:
+# vl53.measurement_timing_budget = 200000
+# The default timing budget is 33ms, a good compromise of speed and accuracy.
+
+# Main loop will read the range and print it every second.
+while True:
+    print("Range: {0}mm".format(vl53.range))
+    time.sleep(1.0)
