7e08df8106edb8aead7db89a6d580974620a83d0
agabay51 <angelagabay3@gmail.com>
2025-03-04 10:48:03 -0700

    Update forceSensorCode.ino

--- ARD_code/forceSensorCode/forceSensorCode.ino
+++ ARD_code/forceSensorCode/forceSensorCode.ino
@@ -3,18 +3,44 @@ Force sensor reading code
 
 Senses the amount of force applied to the linear soft pot reading by reading out an analog voltage
 Reads up to 3N of Force
+Example sketch for SparkFun's soft membrane potentiometer
+  (https://www.sparkfun.com/products/8680)
 
-*/
+- Connect the softpot's outside pins to 5V and GND (the outer pin with an arrow
+indicator should be connected to GND). 
+- Connect the middle pin to A0.
 
-int analogPin = A3; // potentiometer wiper (middle terminal) connected to analog pin 3
-                    // outside leads to ground and +3.3V
-int val = 0;  // variable to store the value read
+As the voltage output of the softpot changes, a line graph printed to the
+serial monitor should match the wiper's position.
 
-void setup() {
-  Serial.begin(9600);           //  setup serial
+Development environment specifics:
+Arduino 1.6.7
+******************************************************************************/
+const int SOFT_POT_PIN = A0; // Pin connected to softpot wiper
+
+const int GRAPH_LENGTH = 40; // Length of line graph
+
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(SOFT_POT_PIN, INPUT);
 }
 
-void loop() {
-  val = analogRead(analogPin);  // read the input pin
-  Serial.println(val);          // debug value
+void loop() 
+{
+  // Read in the soft pot's ADC value
+  int softPotADC = analogRead(SOFT_POT_PIN);
+  // Map the 0-1023 value to 0-40
+  int softPotPosition = map(softPotADC, 0, 1023, 0, GRAPH_LENGTH);
+
+  // Print a line graph:
+  Serial.print("<"); // Starting end
+  for (int i=0; i<GRAPH_LENGTH; i++)
+  {
+    if (i == softPotPosition) Serial.print("|");
+    else Serial.print("-");
+  }
+  Serial.println("> (" + String(softPotADC) + ")");
+
+  delay(500);
 }
