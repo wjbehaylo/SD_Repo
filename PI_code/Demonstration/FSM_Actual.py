fa1a2c9807077fbb4da696acf831c4de791e06fb
wjbehaylo <103539849+wjbehaylo@users.noreply.github.com>
2025-03-11 12:10:01 -0600

    I am concerned that the differing I2C implemenations between the VL53L0X and the I2C_Arduino might create some problems, but that is why we are testing this I suppose.

---
+++ PI_code/Demonstration/FSM_Actual.py
@@ -0,0 +1,12 @@
+# Purpose: this is a demonstrable file that has the currently up to data code on the FSM contained within it
+# Contributors: Walter, Angela
+# Sources: 
+#   Based on programming used in SEED_LAB for python state machine
+# Relevant files: 
+#   UART_Comms.py contains the code for the UART system
+#   CameraCalibAndShapeDetection.py contains the code used for the computer vision
+#   VL53L0X-sensor-code.py contains the code that indicates how the VL53L0X time of flight sensor works
+#   FSM_Outline contains the relevant states that are being used
+# Circuitry: 
+#   for the Pi, follow the circuitry outline in UART_Comms, VL53L0X-sensor-code, CameraCalibAndShapeDetection, and I2C_Arduino (exploration file)
+
