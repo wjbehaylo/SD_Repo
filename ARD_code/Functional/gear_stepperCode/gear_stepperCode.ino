f5a5deba93b34318a6c94301636d74dd19ae54a0
noahkell33 <noahkell33@gmail.com>
2025-04-02 11:55:54 -0600

    Angle function added

--- ARD_code/Functional/gear_stepperCode/gear_stepperCode.ino
+++ ARD_code/Functional/gear_stepperCode/gear_stepperCode.ino
@@ -9,10 +9,9 @@ General: PI Probes ARD for value, sends int response based on status
 Rotational: Rotate to end stop, set position to 0. Get degrees from PI and rotate. Rotate to "plus" from anywhere
 */ 
 
-const float lead_step = 0.01; // 0.01mm
-const int steps_rev = 400; // 1/2 microstep
-// Lead/Revolution = 2mm
 // Steps/Rev = 200 (no microstep)
+const int steps_rev = 400; // 1/2 microstep
+long theta = 0;
 
 AccelStepper stepper_gear1(1,STEPPER3_STEP_PIN,STEPPER3_DIR_PIN);
 
@@ -25,14 +24,20 @@ void setup() {
 
 void loop() {
   
-  stepper_moveMM(stepper_gear1, 2);
+  stepper_moveTheta(stepper_gear1, 90);
+  theta = currentTheta(stepper_gear1);
   delay(1000);
-  stepper_moveMM(stepper_gear1, 0);
+  stepper_moveTheta(stepper_gear1, 0);
+  theta = currentTheta(stepper_gear1);
   delay(1000);
 }
 
-void stepper_moveMM (AccelStepper &stepper, float mm) {
-  float steps = (mm*steps_rev)/(200*lead_step);
+void stepper_moveTheta (AccelStepper &stepper, float theta) {
+  float steps = theta*steps_rev/360;
   stepper.moveTo(steps);
   stepper.runToPosition();
+}
+
+long currentTheta (AccelStepper &stepper) {
+  return long(stepper.currentPosition()*360/steps_rev);
 }
