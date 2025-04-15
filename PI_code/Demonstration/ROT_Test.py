9cc612c7c0efba113efa3716d48174fbe0ec57d3
wjbehaylo <103539849+wjbehaylo@users.noreply.github.com>
2025-04-15 09:15:07 -0600

    Committed walter changes to Rot_Comms.ino logic as Walter_Rot_Comms.ino. Also began writing ROT_Test.py file to test I2C integration and basic system integration. Next I will have to finish the FSM and make sure that works/makes sense, but this is a specific area of functionality thta would be nice to demonstrate.

---
+++ PI_code/Demonstration/ROT_Test.py
@@ -0,0 +1,19 @@
+# Purpose: just to test the functinoality of the rotational arduino communications, without needing to mess with the FSM
+# Contributors: Walter, Angela
+# Sources: 
+#   Based on programming used in SEED_LAB for python state machine
+#   ARD_Comms has the functions we need to send and receive from the arduino
+#   Generate_Status prints out vectors from integer status codes being used
+#   UART_Comms.py has the threaded functionality that lets the system communicate with UART
+# Relevant files: 
+#   UART_Comms.py contains the code for the UART system
+#   ARD_Comms has the functions we need to send and receive from the arduino
+#   Generate_Status prints out vectors from integer status codes being used
+#   Rot_Comms.ino contains the Arduino code that will be running
+# Circuitry: 
+#   for the Pi, follow the circuitry outline in UART_Comms, and I2C_Arduino (exploration file)
+#   For the Arduino, wire it to the pins and stuff as outlined in Rot_Comms.ino
+
+
+
+
