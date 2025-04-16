002cb064b9073bd8460c630bd14ab9f2a7dc74c0
agabay51 <angelagabay3@gmail.com>
2025-04-16 17:51:38 -0600

    doing stuff

--- PI_code/Functional/templateMatchingObjDetection/templateObjectDetect.py
+++ PI_code/Functional/templateMatchingObjDetection/templateObjectDetect.py
@@ -12,7 +12,7 @@ camera = cv2.VideoCapture(0)
 camera.set(cv.CAP_PROP_FRAME_WIDTH, WIDTH)
 camera.set(cv.CAP_PROP_FRAME_HEIGHT, HEIGHT)
 
-template_paths = ['RocketBody.jpg', 'CubeSat.png', 'Starlink.jpg']
+template_paths = ['RocketBody.JPG', 'CubeSat.png', 'Starlink.JPG']
 templates = [cv2.imread(template_path, 0) for template_path in template_paths]
 
 
@@ -26,41 +26,31 @@ while True:
     if not ret:
         print("FAILED!")
         break
-
-# capture frames from the camera
-for frame in camera.capture_continuous(rawCapture, format="bgr",
-                                       use_video_port=True):
-    # grab the raw NumPy array representing the image,
-    # then initialize the timestamp
-    # and occupied/unoccupied text
-    image = frame.array
-
-    # we do something here
-    # we get the image or something then run some matching
-    # if we get a match, we draw a square on it or something
-    img_rbg = image
-
-    img_gray = cv2.cvtColor(img_rbg, cv2.COLOR_BGR2GRAY)
+        
+    img_rgb = frame
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
 
     for template in templates:
         w, h = template.shape[::-1]
-        
+
+        # Template Matching
         res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
-        
-        threshold = 0.8
+
+        threshold = 0.8  # You can adjust this threshold
         loc = np.where(res >= threshold)
 
         for pt in zip(*loc[::-1]):
-            cv2.rectangle(image, (pt[1]. pt[0]), (pt[1] + w, pt[0] + h),
-                      (0,0,255), 2)
+            cv2.rectangle(img_rgb, (pt[0], pt[1]), (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
 
-    # show the frame
-    cv2.imshow("Frame", img_rbg)
+    # show the frame with matches from all templates
+    cv2.imshow("Frame", img_rgb)
+
     key = cv2.waitKey(1) & 0xFF
 
-    # clear the stream in preparation for the next frame
-    #rawCapture.truncate(0)
-
-    # if the `q` key was pressed, break from the loop
+    # if the 'q' key was pressed, break from the loop
     if key == ord("q"):
         break
+
+# release the webcam and close windows
+camera.release()
+cv2.destroyAllWindows()
