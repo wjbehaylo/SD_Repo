3d808626381c7e230051a531d8b4a4826923befd
noahkell33 <noahkell33@gmail.com>
2025-04-17 08:24:25 -0600

    Joystick code for demo

--- ARD_code/Functional/gear_stepperCode/gear_stepperCode.ino
+++ ARD_code/Functional/gear_stepperCode/gear_stepperCode.ino
@@ -3,11 +3,19 @@
 #define STEPPER3_STEP_PIN 6
 #define STEPPER3_DIR_PIN 7
 #define ROT_ARD_ADD 8
+#define joyxPin A0
 
 /* COM NOTES
 General: PI Probes ARD for value, sends int response based on status
 Rotational: Rotate to end stop, set position to 0. Get degrees from PI and rotate. Rotate to "plus" from anywhere
 */ 
+
+int xVal;
+int xSpeed;
+
+const int threshold = 15;
+const int maxSpeed = 100;
+const int maxAccel = 100;
 
 // Steps/Rev = 200 (no microstep)
 const int steps_rev = 400; // 1/2 microstep
@@ -20,20 +28,35 @@ AccelStepper stepper_gear1(1,STEPPER3_STEP_PIN,STEPPER3_DIR_PIN);
 
 void setup() {
   // Declare pins as output
-  stepper_gear1.setMaxSpeed(500);
-  stepper_gear1.setAcceleration(500);
+  Serial.begin(9600);
+  stepper_gear1.setMaxSpeed(maxSpeed);
+  stepper_gear1.setAcceleration(maxAccel);
   stepper_gear1.setCurrentPosition(0);
 }
 
 void loop() {
   
-  theta_d = 90;
-  stepper_rotate(stepper_gear1);
-  delay(1000);
+  //* Function test code *//
+  // theta_d = 90;
+  // stepper_rotate(stepper_gear1);
+  // delay(1000);
 
-  theta_d = 0;
-  stepper_rotate(stepper_gear1);
-  delay(1000);
+  // theta_d = 0;
+  // stepper_rotate(stepper_gear1);
+  // delay(1000);
+
+  //* Joystick code for demo *//
+  xVal = analogRead(joyxPin); // 0-900
+  xSpeed = map(xVal, 0, 900, maxSpeed, -1*maxSpeed);
+
+  // Apply dead zone
+  if (abs(xSpeed) < threshold){
+    xSpeed = 0;
+  }
+
+  stepper_gear1.setSpeed(xSpeed);
+
+  stepper_gear1.runSpeed();
 }
 
 //How many steps is it to move the upper part 1 degree? 0.1 degree?
