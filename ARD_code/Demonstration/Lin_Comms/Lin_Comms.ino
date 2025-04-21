0d8355a4d1e643a415928c10afac75572bfdc8a5
wjbehaylo <103539849+wjbehaylo@users.noreply.github.com>
2025-04-21 00:38:13 -0600

    Main issue seems to be with the immediate prompting of the I2C line right after the PiDaataReceive function informs of the offset that will be used in the data request part of the function

--- ARD_code/Demonstration/Lin_Comms/Lin_Comms.ino
+++ ARD_code/Demonstration/Lin_Comms/Lin_Comms.ino
@@ -197,8 +197,8 @@ void loop() {
  //the functionality varies depending on what we are actively doing
  
  //debugging
- Serial.println(state); //0 is waiting for message, 1 is moving, 2 is done
- delay(3000); //wait 3 seconds between this, just for debugging
+ //Serial.println(state); //0 is waiting for message, 1 is moving, 2 is done
+ //delay(3000); //wait 3 seconds between this, just for debugging
  
  
   switch(state){
@@ -660,7 +660,7 @@ void PiDataReceive(){
     }
     //
     else if(offset==3 || offset==4 || offset==5){
-        Serial.println("Preparing to be read");
+        //Serial.println("Preparing to be read");
         return;
     }
     else{
